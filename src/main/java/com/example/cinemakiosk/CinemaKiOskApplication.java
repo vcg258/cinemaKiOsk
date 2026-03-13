e30ff948fd0ebd30f33298a0f7f6297242af1461
vcg258 <vcg258@naver.com>
2026-03-13 14:49:46 +0900

    26-03-13

---
+++ src/main/java/com/example/cinemakiosk/CinemaKiOskApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemakiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaKiOskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaKiOskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cinemakiosk/CinemaKiOskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinemakiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaKiOskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
