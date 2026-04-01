2513da5b81532911bc2006b40e4efc5cb09f5819
warmice8226 <warmice8226@gmail.com>
2026-04-01 14:24:22 +0900

    mapper processing

--- src/main/java/com/example/cinemakiosk/mapper/SeatPolicyMapper.java
+++ src/main/java/com/example/cinemakiosk/mapper/SeatPolicyMapper.java
@@ -1,7 +1,15 @@
 package com.example.cinemakiosk.mapper;
 
+import com.example.cinemakiosk.vo.SeatPolicyVO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface SeatPolicyMapper {
+    void insert(SeatPolicyVO seatPolicyVO);
+
+    SeatPolicyVO selectOne(Long no);
+
+    SeatPolicyVO selectAll();
+
+
 }
