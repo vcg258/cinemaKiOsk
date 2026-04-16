6664324532cee159b7fd7513eb7389f29bd64e90
vcg2582 <vcg258@naver.com>
2026-04-16 17:43:09 +0900

    26-04-16
    Movie Data ADD

--- src/main/resources/DB/data.sql
+++ src/main/resources/DB/data.sql
@@ -75,24 +75,105 @@ VALUES (1, 10, 1),
        (3, 30, 3),
        (4, 40, 4);
 
-insert ignore into movie (movie_id, actors, create_at, description, director, end_at, genre, rating, runtime, start_at,
-                          title)
-VALUES (1, '연기자 목록', current_timestamp, '설명임', '감독', date_add(current_date, interval 1 day), '액션', 'ALL', 200,
-        date_sub(current_date, interval 6 day), '실험용 영화 제목1'),
-       (2, '연기자 목록', current_timestamp, '설명임', '감독', date_add(current_date, interval 2 day), '로맨스', 'ALL', 200,
-        date_sub(current_date, interval 5 day), '실험용 영화 제목2'),
-       (3, '연기자 목록', current_timestamp, '설명임', '감독', date_add(current_date, interval 3 day), '공포', 'ALL', 200,
-        date_sub(current_date, interval 4 day), '실험용 영화 제목3'),
-       (4, '연기자 목록', current_timestamp, '설명임', '감독', date_add(current_date, interval 4 day), '스릴러', 'ALL', 200,
-        date_sub(current_date, interval 3 day), '실험용 영화 제목4'),
-       (5, '연기자 목록', current_timestamp, '설명임', '감독', date_add(current_date, interval 5 day), '드라마', 'ALL', 200,
-        date_sub(current_date, interval 2 day), '실험용 영화 제목5'),
-       (6, '연기자 목록', current_timestamp, '설명임', '감독', date_add(current_date, interval 6 day), '애니', 'ALL', 200,
-        date_sub(current_date, interval 1 day), '실험용 영화 제목6'),
-       (7, '연기자 목록', current_timestamp, '설명임', '감독', date_add(current_date, interval 14 day), '애니', 'ALL', 200,
-        date_add(current_date, interval 7 day), '실험용 영화 제목7'),
-       (8, '연기자 목록', current_timestamp, '설명임', '감독', date_add(current_date, interval 15 day), '애니', 'ALL', 200,
-        date_add(current_date, interval 8 day), '실험용 영화 제목8');
+-- 1. 인터스텔라
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '인터스텔라', 'SF', '12', 169, '크리스토퍼 놀란', '매슈 매코너히, 앤 해서웨이', '우주를 넘어 인류의 미래를 향한 위대한 여정', '2026-03-01', '2026-05-30', CURDATE(), 'https://image.tmdb.org/t/p/w500/gDN2NWtHbs8ZWEBQM8Dh5OVXdb4.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '인터스텔라' AND start_at = '2026-03-01');
+
+-- 2. 더 퍼스트 슬램덩크
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '더 퍼스트 슬램덩크', '애니메이션', 'ALL', 124, '이노우에 다케히코', '강백호, 서태웅, 채치수', '북산고의 전국 최강 팀과의 결전', '2026-04-01', '2026-06-30', CURDATE(), 'https://image.tmdb.org/t/p/w500/coiJrdXAXuBkSGDvp9bZ7mkuU6E.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '더 퍼스트 슬램덩크' AND start_at = '2026-04-01');
+
+-- 3. 다크 나이트 라이즈
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '다크 나이트 라이즈', '액션', '12', 164, '크리스토퍼 놀란', '크리스찬 베일, 톰 하디', '고담시를 구하기 위한 배트맨의 마지막 사투', '2026-03-15', '2026-05-15', CURDATE(), 'https://image.tmdb.org/t/p/w500/eq5P71YHwwaacEYKaQ72Wxy6BlT.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '다크 나이트 라이즈' AND start_at = '2026-03-15');
+
+-- 4. 기생충
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '기생충', '드라마', '15', 132, '봉준호', '송강호, 이선균, 조여정', '두 가족의 만남이 만들어낸 예측 불가의 이야기', '2026-04-10', '2026-07-10', CURDATE(), 'https://image.tmdb.org/t/p/w500/jjHccoFjbqlfr4VGLVLT7yek0Xn.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '기생충' AND start_at = '2026-04-10');
+
+-- 5. 어벤져스: 인피니티 워
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '어벤져스: 인피니티 워', '액션', '12', 149, '앤서니 루소, 조 루소', '로버트 다우니 주니어, 크리스 에반스', '전 우주를 건 어벤져스의 최대 전투', '2026-04-01', '2026-08-01', CURDATE(), 'https://image.tmdb.org/t/p/w500/kmP6viwzcEkZeoi1LaVcQemcvZh.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '어벤져스: 인피니티 워' AND start_at = '2026-04-01');
+
+-- 6. 헤어질 결심
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '헤어질 결심', '멜로', '15', 138, '박찬욱', '박해일, 탕웨이', '산 위에서 발견된 시신, 그 아내를 향한 형사의 감정', '2026-03-20', '2026-05-20', CURDATE(), 'https://image.tmdb.org/t/p/w500/rXEJ28XDQsogIGqwVEgwM2oDdpl.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '헤어질 결심' AND start_at = '2026-03-20');
+
+-- 7. 범죄도시 4
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '범죄도시 4', '범죄', '15', 109, '허명행', '마동석, 김무열', '마석도가 또 다시 나쁜 놈들을 쓸어버린다', '2026-04-20', '2026-06-20', CURDATE(), 'https://image.tmdb.org/t/p/w500/jucHQwnRSma1O9V2bM007e4eSd7.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '범죄도시 4' AND start_at = '2026-04-20');
+
+-- 8. 신과함께: 죄와 벌
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '신과함께: 죄와 벌', '판타지', '12', 139, '김용화', '하정우, 차태현, 주지훈', '사후 세계 49일간의 7번의 재판', '2026-04-05', '2026-07-05', CURDATE(), 'https://image.tmdb.org/t/p/w500/z54zrEXAKd5u1SCPNOIAKgvGXte.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '신과함께: 죄와 벌' AND start_at = '2026-04-05');
+
+-- 9. 듄: 파트 2
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '듄: 파트 2', 'SF', '12', 166, '드니 빌뇌브', '티모시 샬라메, 젠데이아', '폴 아트레이데스의 운명과 아라키스의 전쟁', '2026-03-01', '2026-05-30', CURDATE(), 'https://image.tmdb.org/t/p/w500/8AsDR2o5AC3V8Jmj6JH6cpta7dz.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '듄: 파트 2' AND start_at = '2026-03-01');
+
+-- 10. 부산행
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '부산행', '호러', '15', 118, '연상호', '공유, 정유미, 마동석', '좀비 바이러스가 창궐한 열차 안에서의 생존기', '2026-04-15', '2026-06-15', CURDATE(), 'https://image.tmdb.org/t/p/w500/6XvEZVBFFjybvb1yQd1qfOC6F2S.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '부산행' AND start_at = '2026-04-15');
+
+-- 11. 탑건: 매버릭
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '탑건: 매버릭', '액션', '12', 130, '조셉 코신스키', '톰 크루즈, 마일스 텔러', '30년 만에 다시 소환된 전설의 파일럿', '2026-03-10', '2026-06-10', CURDATE(), 'https://image.tmdb.org/t/p/w500/jeqXUwNilvNqNXqAHsdwm5pEfae.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '탑건: 매버릭' AND start_at = '2026-03-10');
+
+-- 12. 곡성
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '곡성', '미스터리', '15', 156, '나홍진', '곽도원, 황정민, 천우희', '외지인이 나타난 뒤 마을을 덮친 기이한 사건들', '2026-04-25', '2026-06-25', CURDATE(), 'https://image.tmdb.org/t/p/w500/k9AKtgRErXjz14lFHL2IJVCgwOT.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '곡성' AND start_at = '2026-04-25');
+
+-- 13. 너의 이름은
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '너의 이름은', '애니메이션', 'ALL', 106, '신카이 마코토', '카미키 류노스케, 카미시라이시 모네', '서로의 몸이 뒤바뀐 두 소년소녀의 신비로운 이야기', '2026-04-01', '2026-05-30', CURDATE(), 'https://image.tmdb.org/t/p/w500/2DJCufz3Oa703PbLjNX1pM6MCG2.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '너의 이름은' AND start_at = '2026-04-01');
+
+-- 14. 한산: 용의 출현
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '한산: 용의 출현', '사극', '12', 129, '김한민', '박해일, 변요한', '이순신 장군의 한산도 대첩', '2026-03-25', '2026-05-25', CURDATE(), 'https://image.tmdb.org/t/p/w500/9zPOnQfvWJde1bLR9Y6DF1WUlnJ.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '한산: 용의 출현' AND start_at = '2026-03-25');
+
+-- 15. 조커
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '조커', '스릴러', '15', 122, '토드 필립스', '호아킨 피닉스, 로버트 드 니로', '광기로 물들어가는 한 남자의 비극적 탄생', '2026-04-01', '2026-07-01', CURDATE(), 'https://image.tmdb.org/t/p/w500/6OnFzi7nU6t4j1rmX9QI8EYDWb4.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '조커' AND start_at = '2026-04-01');
+
+-- 16. 인셉션
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '인셉션', 'SF', '12', 148, '크리스토퍼 놀란', '레오나르도 디카프리오, 조셉 고든-레빗', '꿈속에 침투해 생각을 훔치는 자들의 이야기', '2026-03-05', '2026-05-05', CURDATE(), 'https://image.tmdb.org/t/p/w500/atSxEGstxXRoSKDQFBgqQ5lpGSt.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '인셉션' AND start_at = '2026-03-05');
+
+-- 17. 매트릭스: 리저렉션
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '매트릭스: 리저렉션', 'SF', '15', 148, '라나 워쇼스키', '키아누 리브스, 캐리-앤 모스', '다시 소환된 네오, 현실과 가상의 경계에서', '2026-04-12', '2026-06-12', CURDATE(), 'https://image.tmdb.org/t/p/w500/AvWlbyLk32HdvSrrSTYpjmXHEXC.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '매트릭스: 리저렉션' AND start_at = '2026-04-12');
+
+-- 18. 올드보이
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '올드보이', '스릴러', '19', 120, '박찬욱', '최민식, 유지태, 강혜정', '15년 감금 후 풀려난 남자의 복수와 진실', '2026-03-15', '2026-05-15', CURDATE(), 'https://image.tmdb.org/t/p/w500/e8xZvpCS2cHIPSc5WojNaSNvJJS.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '올드보이' AND start_at = '2026-03-15');
+
+-- 19. 라이온 킹
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '라이온 킹', '가족', 'ALL', 118, '존 파브로', '도널드 글로버, 비욘세', '왕의 자리를 되찾기 위한 심바의 여정', '2026-04-10', '2026-07-10', CURDATE(), 'https://image.tmdb.org/t/p/w500/9Y048zYw66TWvpUtsiNK0uReiVX.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '라이온 킹' AND start_at = '2026-04-10');
+
+-- 20. 반지의 제왕: 반지 원정대
+INSERT INTO movie (title, genre, rating, runtime, director, actors, description, start_at, end_at, create_at, poster_path)
+SELECT '반지의 제왕: 반지 원정대', '판타지', '12', 178, '피터 잭슨', '일라이저 우드, 이언 매켈런', '절대반지를 파괴하기 위한 원정대의 출발', '2026-04-01', '2026-08-01', CURDATE(), 'https://image.tmdb.org/t/p/w500/r18JdjImbWDRwkbVDIzWoLQqkCo.jpg'
+FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM movie WHERE title = '반지의 제왕: 반지 원정대' AND start_at = '2026-04-01');
 
 
 -- 스케쥴 등록. 해당 매커니즘은 타이밍을 설정 하면 해당 영화의 상영일에 맞춰서 전부 추가되는 형식.
