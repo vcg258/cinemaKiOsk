c445fb4064b10967c55972ac62167398ab342ddb
vcg258 <vcg258@naver.com>
2026-04-29 17:11:57 +0900

    26-04-29
    SpringAI Marge

---
+++ src/main/java/com/example/cinemakiosk/dto/Tmdb/TmdbCredits/CastDTO.java
@@ -0,0 +1,11 @@
+package com.example.cinemakiosk.dto.Tmdb.TmdbCredits;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CastDTO {
+    private String name;
+}
---
+++ src/main/java/com/example/cinemakiosk/dto/Tmdb/TmdbCredits/CrewDTO.java
@@ -0,0 +1,11 @@
+package com.example.cinemakiosk.dto.Tmdb.TmdbCredits;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CrewDTO {
+    private String name;
+    private String job;
+}
