754fd18594c1b7fbed523b0c7ca91edf7f9b20b1
vcg258 <vcg258@naver.com>
2026-05-02 23:30:14 +0900

    26-05-02

--- src/main/java/com/example/cinemakiosk/service/MemberServiceImpl.java
+++ src/main/java/com/example/cinemakiosk/service/MemberServiceImpl.java
@@ -212,14 +212,4 @@ public class MemberServiceImpl implements MemberService{
         }
         memberRepository.save(member);
     }
-
-    /**
-     * 회원 포인트 갱신하는 기능
-     * @param memberDTO
-     */
-    @Override
-    public void updateMember(MemberDTO memberDTO) {
-        MemberEntity entity = MemberDTO.toEntity(memberDTO);
-        memberRepository.save(entity);
-    }
 }
